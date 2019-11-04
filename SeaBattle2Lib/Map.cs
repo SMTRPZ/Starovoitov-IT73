b42cb0b4bf24d7f3ac16632a27a275eedd6e0634
Ruslan Starovoitov <starovoytov.ruslan@gmail.com>
2019-11-04 11:04:18 +0200

    ползём

---
+++ SeaBattle2Lib/Map.cs
@@ -0,0 +1,7 @@
+﻿namespace SeaBattle2Lib
+{
+    public class Map
+    {
+        
+    }
+}
---
+++ SeaBattle2TelegramServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SeaBattle2TelegramServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
